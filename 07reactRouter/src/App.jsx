80f1069828ee8d9c4e28888602f1ebc084fc76df
Anush <np03cs4a230034@heraldcollege.edu.np>
2025-08-19 14:59:39 +0545

    reactrouter

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+import './App.css'
+import Header from './assets/components/Header/Header'
+import Footer from './assets/components/Footer/Footer'
+import Home from './assets/components/Home/Home'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='background-brown text-green'>hello world</h1>
+
+
+
+    </>
+  )
+}
+
+export default App
